929b4aab3f76dde755ea5796848ef5850f823868
Anne Richardson <richardson.ae@gmail.com>
2019-09-09 18:39:28 -0500

    Finish specs for sequence_number uniqueness

--- spec/models/playlist_pose_spec.rb
+++ spec/models/playlist_pose_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe PlaylistPose, type: :model do
   end
 
   context 'validations' do
-    it { should validate_uniqueness_of(:sequence_number).scoped_to(:playlist_id) }
+    it 'validate_uniqueness_of sequence_number, scoped to playlist' do
+      playlist = create(:playlist)
+      pose = create(:pose)
+      playlist.poses << pose
+
+      should validate_uniqueness_of(:sequence_number).scoped_to(:playlist_id)
+    end
   end
 end
