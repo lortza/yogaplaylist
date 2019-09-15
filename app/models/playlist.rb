70fec310119749193d64316f6ff0a40269eceece
Anne Richardson <richardson.ae@gmail.com>
2019-09-15 18:10:21 -0500

    Add duration to playlist show page

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Playlist < ApplicationRecord
+  SPEAK_TIME_BUFFER_SECONDS = 2
+
   belongs_to :user
   has_many :playlist_poses, dependent: :destroy
   has_many :poses, through: :playlist_poses
@@ -15,6 +17,21 @@ class Playlist < ApplicationRecord
   validates :hold_time,
             presence: true,
             numericality: { greater_than: 0 }
+
+
+  def duration_in_minutes
+    duration_in_seconds / 60
+  end
+
+  def duration_in_seconds
+    qty = poses.count
+    time_per_track = hold_time + speak_time_buffer
+    time_per_track * qty
+  end
+
+  def speak_time_buffer
+    SPEAK_TIME_BUFFER_SECONDS
+  end
 
   def tracks_for_js
     playlist_poses.order(:sequence_number).map(&:audio_file).to_json.html_safe # rubocop:disable Rails/OutputSafety
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -15,4 +15,43 @@ RSpec.describe Playlist, type: :model do
     it { should validate_presence_of(:hold_time) }
     it { should validate_numericality_of(:hold_time).is_greater_than(0) }
   end
+
+  describe '#duration_in_seconds' do
+    it 'returns a second count based on qty of poses and hold time' do
+      playlist = create(:playlist, hold_time: 30)
+      pose = create(:pose)
+      playlist.playlist_poses.create(pose_id: pose.id, sequence_number: 1)
+      playlist.playlist_poses.create(pose_id: pose.id, sequence_number: 2)
+      allow(playlist).to receive(:speak_time_buffer).and_return(0)
+
+      expect(playlist.duration_in_seconds).to eq(60)
+    end
+
+    it 'adds the speak_time_buffer to each pose' do
+      playlist = create(:playlist, hold_time: 20)
+      pose = create(:pose)
+      playlist.playlist_poses.create(pose_id: pose.id, sequence_number: 1)
+      playlist.playlist_poses.create(pose_id: pose.id, sequence_number: 2)
+      allow(playlist).to receive(:speak_time_buffer).and_return(10)
+
+      expect(playlist.duration_in_seconds).to eq(60)
+    end
+  end
+
+  describe '#duration_in_minutes' do
+    it 'returns a minute count based on duration_in_seconds' do
+      playlist = create(:playlist)
+      allow(playlist).to receive(:duration_in_seconds).and_return(120)
+
+      expect(playlist.duration_in_minutes).to eq(2)
+    end
+  end
+
+  describe '#speak_time_buffer' do
+    it 'returns the value of SPEAK_TIME_BUFFER_SECONDS' do
+      playlist = create(:playlist, hold_time: 10)
+
+      expect(playlist.speak_time_buffer).to eq(Playlist::SPEAK_TIME_BUFFER_SECONDS)
+    end
+  end
 end
