d1220e9b410376e324032605eb8d9e2c56156a27
Anne Richardson <richardson.ae@gmail.com>
2019-10-09 19:25:07 -0500

    Add placeholder image

--- app/helpers/poses_helper.rb
+++ app/helpers/poses_helper.rb
@@ -4,4 +4,9 @@ module PosesHelper
   def form_poses(user)
     user.admin? ? Pose.by_name : Pose.public_poses.by_name
   end
+
+  def display_pose_image(pose)
+    file = Rails.application.assets.find_asset(pose.image_file) ? pose.image_file : 'placeholder.png'
+    image_tag file
+  end
 end
